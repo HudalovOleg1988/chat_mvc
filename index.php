721bd7153e83ab87a699b8a64bfaf2988b4cbc85
HudalovOleg1988 <55542403+HudalovOleg1988@users.noreply.github.com>
2024-10-16 16:58:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 
 	session_start();
 
-	// unset($_SESSION['login']);
-	// unset($_SESSION['name']);
-	// unset($_SESSION['email']);
-	// unset($_SESSION['nic']);
-	// unset($_SESSION['user_id']);
-	// unset($_SESSION['avatar']);
-	
 	include $_SERVER['DOCUMENT_ROOT']."/application/db.php";
 	include $_SERVER['DOCUMENT_ROOT']."/application/controllers/controller.php";
 	
