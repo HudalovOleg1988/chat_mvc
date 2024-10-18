ddcb3cfedacb7f8a1f2f8b12017a5a0f8415d74b
HudalovOleg1988 <55542403+HudalovOleg1988@users.noreply.github.com>
2024-10-18 12:53:06 +0300

    Delete application directory

--- application/views/templates/search_form.php
+++
@@ -1,3 +0,0 @@
-<form class="sitebar-search" method="get" action="/<?=Route::$page;?>/index/">
-	<input type="search" name="search" placeholder="<?=$data['placeholder'];?>" value="<?php if(isset($_GET['search'])) echo $_GET['search'];?>">
-</form>
